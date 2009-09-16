c354ad4ee6a0a8c8614c71508f63986ecac0fdc0
Stuart Sierra <mail@stuartsierra.com>
2009-09-16 17:51:32 -0400

    test_status_service_dev.clj: test Application directly, no embedded server
    Faster to test without launching a server.

--- src/test/clojure/org/altlaw/util/restlet/test_status_service_dev.clj
+++ src/test/clojure/org/altlaw/util/restlet/test_status_service_dev.clj
@@ -2,31 +2,35 @@
   (:require org.altlaw.util.restlet.status-service-dev)
   (:use org.altlaw.util.restlet.server
         clojure.test)
-  (:import (org.restlet Restlet)
+  (:import (org.restlet Restlet Application)
            (org.restlet.resource StringRepresentation)
+           (org.restlet.data Request Response Method)
            (org.apache.commons.io IOUtils)
            (java.net URL)))
 
-(defn make-error-app []
+(defn make-error-restlet []
   (proxy [Restlet] []
     (handle [request response]
             (.setEntity response
                         (StringRepresentation. "Hello, World!"))
             (throw (Exception. "This is the error.")))))
 
-(defn check-response []
-  (let [response (IOUtils/toString ;; quick & dirty HTTP client
-                  (.openStream (URL. "http://127.0.0.1:13999/")))]
-    (is (.contains response "HTTP 500"))
-    (is (.contains response "This is the error."))))
+(defn make-status-service []
+  (doto (.. Class
+            (forName "org.altlaw.util.restlet.status_service_dev")
+            newInstance)
+    (.setOverwrite true)))
 
-(deftest t-server-status-dev
-  (let [component (make-component 13999 (make-error-app))]
-    (.setStatusService
-     component
-     (.. Class
-         (forName "org.altlaw.util.restlet.status_service_dev")
-         newInstance))
-    (.start component)
-    (check-response)
-    (stop-component component)))
+(defn make-error-app []
+  (doto (Application.)
+    (.setRoot (make-error-restlet))
+    (.setStatusService (make-status-service))))
+
+(deftest t-status-service-dev
+  (let [app (make-error-app)
+        request (Request. Method/GET "http://host/")
+        response (Response. request)]
+    (.handle app request response)
+    (is (= 500 (.. response getStatus getCode)))
+    (is (.contains (.. response getEntity getText)
+                   "This is the error."))))
